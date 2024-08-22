05e372c6860479c043ce13c0a82230db63c3f357
code kit2.0 <lenny69140@gmail.com>
2024-08-22 22:10:51 +0200

    Update ready.js

--- ready.js
+++ ready.js
@@ -9,7 +9,7 @@ module.exports = (client) => {
       client.user.setPresence({
         activities: [
           {
-            name: 'Status Bot',
+            name: 'status de ton bot',
             type: ActivityType.Streaming, // Utiliser ActivityType.Streaming pour v14
             url: 'https://twitch.tv/kityt69' // Remplace par une URL de streaming
           }
